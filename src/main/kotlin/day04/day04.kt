c49ae26da16d5cf0a9b060acedf259480d1f4dfb
LWLeijten <larsleijten@gmail.com>
2023-12-04 21:15:40 +0100

    Day 4 part 2

--- src/main/kotlin/day04/day04.kt
+++ src/main/kotlin/day04/day04.kt
@@ -8,6 +8,7 @@ import kotlin.math.pow
 fun day4() {
     val fileContent = readInputAsStringLines("day04")
     day4a(fileContent)
+    day4b(fileContent)
 }
 
 private fun day4a(fileContent: List<String>) {
@@ -15,13 +16,33 @@ private fun day4a(fileContent: List<String>) {
     fileContent
         .map { fc -> fc.substringAfter(':') }
         .forEach { m ->
-            val split = m.split("|")
-            val left = split[0].trim().split(WHITESPACE_REGEX)
-            val right = split[1].trim().split(WHITESPACE_REGEX)
-            val matches = left.filter { l -> right.contains(l) }.size
+            val matches = getAmountOfMatches(m)
             if (matches > 0) {
                 sum += 2.0.pow((matches - 1)).toInt()
             }
         }
     println("Part one solution: $sum")
+}
+
+private fun day4b(fileContent: List<String>) {
+    val cardCountMap = mutableMapOf<Int, Int>()
+    (1..fileContent.size).forEach { cardCountMap[it] = 1}
+    for (i in 1..fileContent.size) {
+        val matches = getAmountOfMatches(fileContent[i - 1])
+        if (matches > 0) {
+            for (j in 1..matches) {
+                val oldCount = cardCountMap.getOrDefault(i + j, 1)
+                cardCountMap[i + j] = cardCountMap.getOrDefault(i, 1) + oldCount
+            }
+        }
+    }
+    val result = cardCountMap.values.sum()
+    println("Part one solution: $result")
+}
+
+private fun getAmountOfMatches(row: String): Int {
+    val split = row.split("|")
+    val left = split[0].trim().split(WHITESPACE_REGEX)
+    val right = split[1].trim().split(WHITESPACE_REGEX)
+    return left.filter { l -> right.contains(l) }.size
 }
