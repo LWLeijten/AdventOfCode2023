8dc733c93b3587b42eefc697ab4ad3a1d79d7dbd
LWLeijten <larsleijten@gmail.com>
2023-12-07 18:55:16 +0100

    Day 7 full solution

--- src/main/kotlin/day07/day07.kt
+++ src/main/kotlin/day07/day07.kt
@@ -3,41 +3,54 @@ package day07
 import utils.readInputAsStringLines
 
 val CARD_MAP = mapOf('T' to 10, 'J' to 11, 'Q' to 12, 'K' to 13, 'A' to 14)
+val CARD_MAP_B = mapOf('T' to 10, 'J' to 1, 'Q' to 12, 'K' to 13, 'A' to 14)
 
 fun day7() {
     val fileContent = readInputAsStringLines("day07")
-    val handBids = mutableListOf<HandBid>()
-    fileContent.forEach { fc ->
-        val hand = fc.split(" ")[0].map { mapCharToCardNumber(it) }
-        handBids.add(
-            HandBid(
-                hand,
-                mapHandToGroupSizes(hand),
-                fc.split(" ")[1].toInt()
-            )
-        )
-    }
-    day7a(handBids)
+    val handBidsOne = initHandBids(fileContent, CARD_MAP, ::mapHandToGroupSizes)
+    val handBidsTwo = initHandBids(fileContent, CARD_MAP_B, ::mapHandToGroupSizesTwo)
+    val partOne = solve(handBidsOne)
+    println("Part one solution: $partOne")
+    val partTwo = solve(handBidsTwo)
+    println("Part two solution: $partTwo")
 }
 
-fun day7a(handBids: List<HandBid>) {
-    val sum = handBids.sortedWith(
+private fun solve(handBids: List<HandBid>): Int {
+    return handBids.sortedWith(
         compareBy({ it.cardGroups[0] },
-            {it.cardGroups.getOrElse(1) {0} },
+            { it.cardGroups.getOrElse(1) { 0 } },
             { it.hand[0] },
             { it.hand[1] },
             { it.hand[2] },
             { it.hand[3] },
             { it.hand[4] })
-    ).mapIndexed{ i, hb -> (i + 1) * hb.bid}.sum()
-    println("Part one solution: $sum")
+    ).mapIndexed { i, hb -> (i + 1) * hb.bid }.sum()
 }
 
-private fun mapCharToCardNumber(char: Char): Int {
+private fun initHandBids(
+    fileContent: List<String>,
+    cardMapping: Map<Char, Int>,
+    grouping: (List<Int>) -> List<Int>
+): List<HandBid> {
+    val handBids = mutableListOf<HandBid>()
+    fileContent.forEach { fc ->
+        val hand = fc.split(" ")[0].map { mapCharToCardNumber(it, cardMapping) }
+        handBids.add(
+            HandBid(
+                hand,
+                grouping(hand),
+                fc.split(" ")[1].toInt()
+            )
+        )
+    }
+    return handBids
+}
+
+private fun mapCharToCardNumber(char: Char, cardMapping: Map<Char, Int>): Int {
     return if (char.isDigit()) {
         char.digitToInt()
     } else {
-        CARD_MAP[char]!!
+        cardMapping[char]!!
     }
 }
 
@@ -46,4 +59,28 @@ private fun mapHandToGroupSizes(hand: List<Int>): List<Int> {
         .groupBy { it }
         .map { it.value.size }
         .sortedByDescending { it }
+}
+
+private fun mapHandToGroupSizesTwo(hand: List<Int>): List<Int> {
+    // method could be cleaner, but it works ¯\_(ツ)_/¯
+    val grouped = hand.groupBy { it }
+    var jokers = 0
+    // Calculate amount of jokers + edge case for 5 jokers
+    if (grouped.containsKey(1)) {
+        jokers = grouped[1]!!.size
+        if (jokers == 5) {
+            return arrayListOf(5)
+        }
+    }
+    // filter out jokers
+    val filtered = grouped.filter { it.key != 1 }.toMutableMap()
+    // get the card we should add the jokers to
+    val subst = filtered.toSortedMap(compareByDescending { filtered[it]?.size }).firstKey()
+    // add jokers to the determined card
+    for (i in 0..<jokers) {
+        val copy = filtered[subst]?.toMutableList()!!
+        copy.add(subst)
+        filtered[subst] = copy
+    }
+    return filtered.map { it.value.size }.sortedByDescending { it }
 }
